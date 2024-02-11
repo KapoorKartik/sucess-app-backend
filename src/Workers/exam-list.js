771adb9c091bbcb183382a280d2c7a654fb8410c
kapoorkartik <kapoorkartik110@gmail.com>
2024-02-11 20:51:48 +0530

    update

--- src/Workers/exam-list.js
+++ src/Workers/exam-list.js
@@ -1,35 +1,29 @@
 const { workerData, parentPort } = require("worker_threads");
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 async function getAllCollections() {
-  console.log("getAllCollections:");
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
   try {
-    parentPort.postMessage({ result: "msg from worker " + workerData });
+    await client.connect();
+
+    const filter = {};
+    const sort = {
+      _id: 1,
+    };
+
+    const coll = client.db("success-app").collection("exam-list");
+    const cursor = coll.find(filter, { sort });
+    const result = await cursor.toArray();
+
+    parentPort.postMessage({ result: result });
   } catch (error) {
+    console.error("Error in worker:", error.message);
     parentPort.postMessage({ error: error.message });
+  } finally {
+    await client.close();
   }
-  // const uri = "mongodb://localhost:27017";
-  // const client = new MongoClient(uri);
-
-  // try {
-  //   await client.connect();
-
-  //   const filter = {};
-  //   const sort = {
-  //     _id: 1,
-  //   };
-
-  //   const coll = client.db("success-app").collection("exam-list");
-  //   const cursor = coll.find(filter, { sort });
-  //   const result = await cursor.toArray();
-
-  //   parentPort.postMessage({ result: result });
-  // } catch (error) {
-  //   console.error("Error in worker:", error.message);
-  //   parentPort.postMessage({ error: error.message });
-  // } finally {
-  //   await client.close();
-  // }
 }
 
 // Call the function to get all collections
