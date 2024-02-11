38a91d31a5a3ca79bce86956b1387077c04bc00f
kapoorkartik <kapoorkartik110@gmail.com>
2024-02-11 20:48:44 +0530

    understanding worker

--- src/index.js
+++ src/index.js
@@ -4,14 +4,49 @@ const express = require("express");
 const { Worker } = require("worker_threads");
 const app = express();
 
+
+app.get("/",(req,res)=>{
+  res.send("Working on port 80")
+  try {
+    require.resolve("worker_threads");
+    console.log("worker_threads module is present");
+  } catch (error) {
+    console.error("worker_threads module is not present");
+  }
+})
 app.get("/api/exam-list/", async (req, res) => {
-  const worker = new Worker("./Workers/exam-list.js");
+  // const worker = new Worker("./Workers/exam-list.js");
+  // worker.on("message", (data) => {
+  // res.send("data");
+  // });
+  // worker.on("error", (msg) => {
+  // res.send(msg);
+  // });
+  console.log('worker0:')
+  const tick = performance.now()
+  const worker = new Worker("./src/Workers/exam-list.js", {
+    workerData: "worker1",
+  });
+  const worker2 = new Worker("./src/Workers/exam-list.js", {
+    workerData: "worker2",
+  });
   worker.on("message", (data) => {
-    res.send(data);
+    // res.send(data);
+    console.log('data:', data)
   });
   worker.on("error", (msg) => {
+  res.send(msg);
+  });
+  worker2.on("message", (data) => {
+    // res.send(data);
+    console.log('data:', data)
+  });
+  worker2.on("error", (msg) => {
     res.send(msg);
   });
+  const tok = performance.now()
+  const time = tok - tick
+  console.log('time:', time + ' ms')
 });
 
 app.get("/api/exam-data/:id", async (req, res) => {
