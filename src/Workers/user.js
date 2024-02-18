480d2ae37e15af4bff6cf45c4f8a673f50fbad9e
kapoorkartik <kapoorkartik110@gmail.com>
2024-02-18 22:18:20 +0530

    little change

--- src/Workers/user.js
+++ src/Workers/user.js
@@ -1,12 +1,13 @@
-const {workerData, parentPort } = require("worker_threads");
+const { workerData, parentPort } = require("worker_threads");
 const { connectToDB, closeDBConnection } = require("../configs/db");
 const mobileNumber = workerData.req;
 const getDataFormDb = async () => {
   try {
     const db = await connectToDB();
     const collection = db.collection("users");
-    const cursor = collection.find({mobileNumber}, { _id: 1 });
-    const res = await cursor.toArray();
+    const res = await collection.findOne({ mobileNumber }, { _id: 1 });
+    console.log("cursor f:", res);
+    // const res = await cursor.toArray();
     parentPort.postMessage({ res });
   } catch (error) {
     console.error("Error in worker:", error.message);
